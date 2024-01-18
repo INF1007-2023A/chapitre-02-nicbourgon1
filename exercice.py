06c2487b7d22f178283b30a33832841e018d881b
nicbourgon1 <nicholas.bourgon@polymtl.ca>
2024-01-18 14:53:49 -0500

    Exercice complété

--- exercice.py
+++ exercice.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 def majuscule(mot):
-    # TODO completer la fonction ici
-    return mot
+    nouveau_mot = ''
+    for x in mot:
+        if 97 <= ord(x) <= 122:
+            nouveau_mot += chr(ord(x) - 32)
+        elif 65 <= ord(x) <= 90:
+            nouveau_mot += chr(ord(x) + 32)
+        else:
+            nouveau_mot += x
+    return nouveau_mot
 
 
 if __name__ == '__main__':
